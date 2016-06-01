4a61b3ad2bbb17f2f4c333b0d596be27eab0a525
bmanjuu <bettyofung@gmail.com>
2016-06-01 14:16:09 -0400

    third pass at deli counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,35 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine{
+    NSInteger deliLineLength = [deliLine count];
+    
+    NSMutableString *lineInfo = [@"The line is:" mutableCopy];
+    if(deliLineLength > 0){
+        for(NSUInteger i = 0; i < deliLineLength; i++){
+            [lineInfo appendFormat:@"\n%lu. %@", i+1, deliLine[i]];
+        }
+    }
+    else {
+        return @"The line is currently empty.";
+    }
+    return lineInfo;
+}
+
+
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    [deliLine addObject:name]; 
+}
+
+
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+    
+    NSString *firstCustomer = [deliLine firstObject];
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstCustomer;
+}
 
 @end
